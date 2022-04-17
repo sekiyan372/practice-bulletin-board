b57c8dbbc2021aae36e63f77acf488caf81fb712
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-04-17 13:40:09 +0900

    feat: modal

--- src/components/Card/PhotoconCard.tsx
+++ src/components/Card/PhotoconCard.tsx
@@ -1,7 +1,7 @@
-import { Box, Text } from '@chakra-ui/react'
-import Image from 'next/image'
+import { Box, Image, Text, useDisclosure } from '@chakra-ui/react'
 import type { FC } from 'react'
 
+import { PhotoconModal } from '~/components/Modal'
 import { Photocon } from '~/types'
 
 type Props = {
@@ -10,28 +10,21 @@ type Props = {
 }
 
 export const PhotoconCard: FC<Props> = ({ photocon, index }) => {
-  console.log(photocon.image)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Box textAlign="center" p="10px">
+    <Box textAlign="center" p="10px" pb="50px">
+      <Image
+        src={photocon.image}
+        alt="Picture of photo contest"
+        height={200}
+        onClick={onOpen}
+      />
       <Text fontSize="30px" fontWeight="bold">
         {index}
       </Text>
-      <Box border="2px" borderColor="main.red" p="5px">
-        <Image
-          src={photocon.image}
-          alt="Picture of photo contest"
-          height={200}
-          width={350}
-          // layout="fill"
-          // objectFit="contain"
-        />
-        <Text fontSize="20px" color="gray.800">
-          {photocon.name}
-        </Text>
-        <Text fontSize="16px" color="gray.600">
-          {photocon.comment}
-        </Text>
-      </Box>
+
+      <PhotoconModal photocon={photocon} isOpen={isOpen} onClose={onClose} />
     </Box>
   )
 }
