b704537710ebcf537f77c0ad508c85e2c766b50a
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-04-17 21:02:17 +0900

    feat: add bass settings

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,5 @@
 export default {
+  '*.{ts, tsx}': () => 'tsc -p tsconfig.build.json',
   '*.{js,ts,jsx,tsx}':
     'eslint --cache --cache-location .next/cache/eslint/ --fix',
   '*': 'prettier --write .',
