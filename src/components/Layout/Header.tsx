674bf6f7bcfd76a754fa688f04b5fee54a6c98cf
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-06-27 07:53:29 +0900

    feat: responsive header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,10 +1,65 @@
-import { chakra } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
 import type { FC } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 export const Header: FC = () => {
   return (
-    <chakra.header bg="main.red" color="white" fontSize="30px" p="10px">
-      撮っテク！管理者ページ
-    </chakra.header>
+    <Flex
+      bg="main.red"
+      color="white"
+      minWidth="max-content"
+      alignItems="center"
+      gap="2"
+      p="4"
+    >
+      <Heading fontSize="30px">
+        <NextLink href="/">撮っテク！管理者ページ</NextLink>
+      </Heading>
+
+      <Spacer />
+      <Box gap="2" display={{ base: 'none', lg: 'block' }}>
+        <Box display="inline-block" px="10">
+          <NextLink href="/">ホーム</NextLink>
+        </Box>
+        <Box display="inline-block" px="10">
+          <NextLink href="/album">アルバム</NextLink>
+        </Box>
+        <Box display="inline-block" px="10">
+          <NextLink href="/contest">フォトコンテスト</NextLink>
+        </Box>
+      </Box>
+
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Options"
+          icon={<GiHamburgerMenu />}
+          variant="outline"
+          display={{ base: 'flex', lg: 'none' }}
+        />
+        <MenuList color="black">
+          <MenuItem>
+            <NextLink href="/">ホーム</NextLink>
+          </MenuItem>
+          <MenuItem>
+            <NextLink href="/album">アルバム</NextLink>
+          </MenuItem>
+          <MenuItem>
+            <NextLink href="/contest">フォトコンテスト</NextLink>
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </Flex>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Heading, List, ListIcon, ListItem } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { FaAward } from 'react-icons/fa'
 import { MdPhotoAlbum } from 'react-icons/md'
 
@@ -10,15 +10,15 @@ const Home: NextPage = () => {
       <Heading textAlign="center" m="50px" color="gray.800">
         撮っテク！管理ページ
       </Heading>
-      <Box textAlign="center">
-        <List spacing={3}>
+      <Box textAlign="center" pb="12">
+        <List spacing="3">
           <ListItem>
             <ListIcon as={MdPhotoAlbum} />
-            <Link href="/album">アルバム管理</Link>
+            <NextLink href="/album">アルバム管理</NextLink>
           </ListItem>
           <ListItem>
             <ListIcon as={FaAward} />
-            <Link href="/contest">フォトコンテスト管理</Link>
+            <NextLink href="/contest">フォトコンテスト管理</NextLink>
           </ListItem>
         </List>
       </Box>
