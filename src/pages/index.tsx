3d728b54f913430f8b35959f60579e78e16e70cb
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-07-10 19:29:11 +0900

    feat: markdown in top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,55 @@
+import 'github-markdown-css/github-markdown.css'
+
 import { Box, Heading, List, ListIcon, ListItem } from '@chakra-ui/react'
-import type { NextPage } from 'next'
+import fs from 'fs'
+import type { GetStaticProps, NextPage } from 'next'
 import NextLink from 'next/link'
 import { FaAward } from 'react-icons/fa'
 import { MdPhotoAlbum } from 'react-icons/md'
+import ReactMarkdown from 'react-markdown'
 
-const Home: NextPage = () => {
+type Props = {
+  topMd: string
+}
+
+const Home: NextPage<Props> = ({ topMd }) => {
   return (
     <>
       <Heading textAlign="center" m="50px" color="gray.800">
-        撮っテク！管理ページ
+        撮っテク！管理者ページ
       </Heading>
-      <Box textAlign="center" pb="12">
-        <List spacing="3">
-          <ListItem>
+      <Box textAlign="center" mx="auto" color="white">
+        <List
+          spacing="6"
+          display="inline-block"
+          p="6"
+          borderRadius="6"
+          backgroundColor="main.red"
+        >
+          <ListItem fontSize="20" _hover={{ opacity: 0.5 }}>
             <ListIcon as={MdPhotoAlbum} />
             <NextLink href="/album">アルバム管理</NextLink>
           </ListItem>
-          <ListItem>
+          <ListItem fontSize="20" _hover={{ opacity: 0.5 }}>
             <ListIcon as={FaAward} />
             <NextLink href="/contest">フォトコンテスト管理</NextLink>
           </ListItem>
         </List>
       </Box>
+      <Box p="12">
+        <ReactMarkdown className="markdown-body">{topMd}</ReactMarkdown>
+      </Box>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const topMd = fs.readFileSync(`${process.cwd()}/public/top.md`, 'utf8')
+  return {
+    props: {
+      topMd: topMd,
+    },
+  }
 }
 
 export default Home
